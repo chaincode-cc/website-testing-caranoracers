93943d0ab8bcae865660a5dbdfd69fb25b2801e9
William <williamalvarez672@gmail.com>
2022-09-24 23:14:00 +0100

    nft

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './styles/App.css';
 
 import Banner from './components/banner';
 import Intro from './pages/intro';
+import NFTS from './pages/nft';
 
 function App() {
 return ( 
 <div className="main-wrapper"> 
 <Banner />
 <Intro />
+<NFTS />
 
 </div> 
 ); 
