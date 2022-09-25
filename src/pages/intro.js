d93a39dcd3e03f7a8bd81b6a7d64cc1427ac3117
William <williamalvarez672@gmail.com>
2022-09-25 18:45:06 +0100

    NTF SOME WORK

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './intro.css'
 
 const Intro = () => {
     return(
         <>
-    <h2>What is Cardano Racers?</h2>
-    <p className="">Here is where we talk about what the project is all about.</p>
+    <h2 className="fontAutoSized">What is Cardano Racers?</h2>
+    <p className="fontAutoSized" id='intro'>Here is where we talk about what the project is all about.</p>
         </>
     )
 }
--- src/pages/nft.js
+++ src/pages/nft.js
@@ -1,11 +1,12 @@
 import React from "react";
 import wheel from '../assets/images/wheel.png'
-
+import './nft.css'
 
 const NFTS = () => {
     return(
         <>
-        NFTS
+        <div className="d-flex flex-column carousel-wrapper">
+        <section>
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -13,14 +14,15 @@ const NFTS = () => {
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner">
+          <p>CARS</p>
     <div class="carousel-item active">
-      <img src={wheel} class="d-flex w-50" alt="..." />
+      <img src={wheel} class="d-flex " alt="..." />
     </div>
     <div class="carousel-item">
-      <img src={wheel} class="d-flex w-50" alt="..." />
+      <img src={wheel} class="d-flex " alt="..." />
     </div>
     <div class="carousel-item">
-      <img src={wheel} class="d-flex w-50" alt="..." />
+      <img src={wheel} class="d-flex " alt="..." />
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -32,7 +34,38 @@ const NFTS = () => {
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-   </>
+</section>
+<section>
+        <div id="carouselExampleIndicators1" class="carousel slide" data-bs-ride="true">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+<p>DRIVERS</p>
+    <div class="carousel-item active">
+      <img src={wheel} class="d-flex " alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={wheel} class="d-flex " alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={wheel} class="d-flex " alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators1" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</section>
+</div>
+  </>
     )
 }
 
