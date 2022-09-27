20157fb23ccb3150714abe4d62b40a83def326bc
William <williamalvarez672@gmail.com>
2022-09-27 21:34:26 +0100

    some banner vid work

--- src/pages/nft.js
+++ src/pages/nft.js
@@ -9,7 +9,7 @@ const NFTS = () => {
         <>
   <div class="d-flex align-content-around justify-content-around flex-wrap">
   
-    <div class="card m-2">
+    <div class="card">
       <div class="card-body bg-secondary">
         <h5 class="card-title fontAutoSized">Special title treatment</h5>
         <p class="card-text fontAutoSized">IMG</p>
