7b269faf3fa5c4725aff7a99eabd749ce0ac7503
William <williamalvarez672@gmail.com>
2022-09-27 21:34:59 +0100

    new file:   src/assets/videos/1.gif new file:   src/assets/videos/2.gif new file:   src/assets/videos/3.gif new file:   src/assets/videos/4.gif new file:   src/assets/videos/Car1.mp4 new file:   src/assets/videos/CarPerspective.mp4 new file:   src/assets/videos/Racer1.MP4 new file:   src/assets/videos/SlotMachine.mp4 new file:   src/assets/videos/cool.gif new file:   src/assets/videos/cool.mp4 modified:   src/components/banner.css modified:   src/components/banner.js

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,11 +1,27 @@
+/* eslint-disable react/style-prop-object */
 import React from "react";
 import './banner.css'
 import logo1 from '../assets/images/10-01.png'
+import vid1 from '../assets/videos/cool.gif'
+import vid2 from '../assets/videos/cool.mp4'
+
+
+
 
 const Banner = () => {
     return(
         <>
+  <div>
+  <img src={logo1} alt='some' className="main-logo" />
+<video src={vid2} muted loop autoPlay className="banner-video">
+  Your browser does not support the video tag.
+</video>
+</div>
+      
+      {/* <div className="main-logo" > 
       <img src={logo1} alt='some' className="main-logo" />
+      </div> */}
+      
         </>
     )
 }
