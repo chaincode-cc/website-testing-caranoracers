e9b0102ccf4b9dd21f32735214c4aef69ca8a38f
William <williamalvarez672@gmail.com>
2022-09-28 09:21:49 +0100

    some more css in nft

--- src/App.js
+++ src/App.js
@@ -8,16 +8,24 @@ import NFTS from './pages/nft';
 import HowTo from './pages/howto';
 import Contact from './pages/contact';
 import FollowUs from './pages/followus';
+import CountDown from './components/countdown';
+
+const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+const NOW_IN_MS = new Date().getTime();
+
+const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
 
 function App() {
 return ( 
 <div className="main-wrapper"> 
 <Banner />
+<CountDown targetDate={dateTimeAfterThreeDays} />
 <NFTS />
 <Intro />
 <HowTo />
 <Contact />
 <FollowUs />
+
 </div> 
 ); 
 }; 
--- src/pages/nft.js
+++ src/pages/nft.js
@@ -4,6 +4,8 @@
 import React from "react";
 import './nft.css'
 import nft1 from '../assets/videos/Car1.mp4'
+import nft2 from '../assets/videos/Racer1.MP4'
+
 
 const NFTTitles = {
   Cars:{  
@@ -13,18 +15,21 @@ const NFTTitles = {
   },
   Racer:{  
       one:"Normal Racer",
-      two:"Cool Car",
-      three:"Crazy Car",
+      two:"Cool Racer",
+      three:"Crazy Racer",
   }
 }
 
 const NFTS = () => {
     return(
-        <>
+        <div className="mt-4 text-center">
+        {/* CARS */}
+        <h3>CARS</h3>
   <div class="d-flex align-content-around justify-content-around flex-wrap">
   
     <div class="card bg-secondary d-flex justify-content-center align-items-center">
       <div class="card-body bg-secondary">
+      <div class="ribbon ribbon-top-right"><span>Common</span></div>
         <h5 class="card-title fontAutoSized">{NFTTitles.Cars.one}</h5>
         <video src={nft1} muted loop autoPlay className="nft1">
           Your browser does not support the video tag.
@@ -35,6 +40,7 @@ const NFTS = () => {
   
     <div class="card bg-secondary d-flex justify-content-center align-items-center">
       <div class="card-body bg-secondary">
+      <div class="ribbon ribbon-top-right"><span>uncommon</span></div>
         <h5 class="card-title fontAutoSized">{NFTTitles.Cars.two}</h5>
         <video src={nft1} muted loop autoPlay className="nft1">
           Your browser does not support the video tag.
@@ -46,6 +52,7 @@ const NFTS = () => {
   
     <div class="card bg-secondary d-flex justify-content-center align-items-center">
       <div class="card-body bg-secondary">
+      <div class="ribbon ribbon-top-right"><span>rare</span></div>
         <h5 class="card-title fontAutoSized">{NFTTitles.Cars.three}</h5>
         <video src={nft1} muted loop autoPlay className="nft1">
           Your browser does not support the video tag.
@@ -53,10 +60,51 @@ const NFTS = () => {
       </div>
         <a href="#" class="btn btn-primary fontAutoSized"><span>Buy</span></a>
     </div>
-  
 </div>   
 
-    </>
+{/* DRIVERS */}
+<h3 className="mt-4">RACERS</h3>
+<div class="d-flex align-content-around justify-content-around flex-wrap">
+  
+  <div class="card bg-secondary d-flex justify-content-center align-items-center">
+    <div class="card-body bg-secondary">
+    <div class="ribbon ribbon-top-right"><span>Common</span></div>
+
+      <h5 class="card-title fontAutoSized">{NFTTitles.Racer.one}</h5>
+      <video src={nft2} muted loop autoPlay className="nft2">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+      <a href="#" class="btn btn-primary fontAutoSized"><span>Buy</span></a>
+  </div>
+
+  <div class="card bg-secondary d-flex justify-content-center align-items-center">
+    <div class="card-body bg-secondary">
+    <div class="ribbon ribbon-top-right"><span>uncommon</span></div>
+
+      <h5 class="card-title fontAutoSized">{NFTTitles.Racer.two}</h5>
+      <video src={nft2} muted loop autoPlay className="nft2">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+      <a href="#" class="btn btn-primary fontAutoSized"><span>Buy</span></a>
+  
+</div>
+
+  <div class="card bg-secondary d-flex justify-content-center align-items-center">
+    <div class="card-body bg-secondary">
+    <div class="ribbon ribbon-top-right"><span>rare</span></div>
+
+      <h5 class="card-title fontAutoSized">{NFTTitles.Racer.three}</h5>
+      <video src={nft2} muted loop autoPlay className="nft2">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+      <a href="#" class="btn btn-primary fontAutoSized"><span>Buy</span></a>
+  </div>
+
+</div>   
+    </div>
     )
 }
 
