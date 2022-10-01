2dde64927a95574fe09d70816da86db0fdc5ed6c
William <williamalvarez672@gmail.com>
2022-10-01 23:54:39 +0100

    toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,31 +2,46 @@
 
 function TopNavBar() {
 
-function setToggler () {
-  console.log("Clicked")
-}
+
 
   return (
     <>
-    <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+    <nav className="navbar sticky-top navbar-dark bg-dark">
   <div className="container-fluid" >
-    <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+    <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <a className="navbar-brand f1font" href="#">CARDANO RACERS</a>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav ps-1">
         <li className="nav-item">
-          <a className="nav-link f1font" onClick={setToggler} aria-current="page" href="#intro">HOME</a>
+          <a className="nav-link f1font"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-expanded="false" aria-current="page" href="#">HOME</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link f1font" onClick={setToggler} href="#howtoplay">HOW TO PLAY</a>
+          <a   className="nav-link f1font"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-expanded="false"
+        href="#howtoplay">HOW TO PLAY</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link f1font" onClick={setToggler} href="#nfts">NFTS</a>
+          <a className="nav-link f1font"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-expanded="false" href="#nfts">NFTS</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link f1font" onClick={setToggler} href="#contact">CONTACT US</a>
+          <a className="nav-link f1font"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-expanded="false" href="#contact">CONTACT US</a>
         </li>
         <li className="nav-item">
           <button className="f1font btn btn-secondary" id='garage' data-bs-toggle="modal" data-bs-target="#exampleModal">GARAGE</button>
@@ -42,7 +57,7 @@ function setToggler () {
   <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable " >
     <div className="modal-content" style={{background:"#969696"}}>
       <div className="modal-header" >
-        <h5 className="modal-title f1font"  id="exampleModalLabel">Your Garage</h5>
+        <h5 className="modal-title f1font" id="exampleModalLabel">Your Garage</h5>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
