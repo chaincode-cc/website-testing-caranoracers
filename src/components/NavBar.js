11775ee8e57d15aea85a7a1e1f1716e9a3913e96
William <williamalvarez672@gmail.com>
2022-10-01 22:14:50 +0100

    some work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,8 +9,8 @@ function setToggler () {
   return (
     <>
     <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+  <div className="container-fluid" >
+    <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <a className="navbar-brand f1font" href="#">CARDANO RACERS</a>
@@ -28,10 +28,33 @@ function setToggler () {
         <li className="nav-item">
           <a className="nav-link f1font" onClick={setToggler} href="#contact">CONTACT US</a>
         </li>
+        <li className="nav-item">
+          <button className="nav-link garage btn btn-secondary f1font" data-bs-toggle="modal" data-bs-target="#exampleModal">GARAGE</button>
+        </li>
       </ul>
+    </div>
+    <div className="collapse navbar-collapse" id="navbarNav">
     </div>
   </div>
 </nav>
+
+<div className="modal fade "  id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable " >
+    <div className="modal-content" style={{background:"#969696"}}>
+      <div className="modal-header" >
+        <h5 className="modal-title f1font"  id="exampleModalLabel">Your Garage</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        Here I will ask you to connect your wallet if not connected or I will show you your garage with your NFTs.
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     </>
   );
 }
