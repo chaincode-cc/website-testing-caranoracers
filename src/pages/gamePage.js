4220880f3d0fc3d0958d682dc5393e1da79e9777
William Alvarez <williamalvarez672@gmail.com>
2022-12-15 16:02:16 +0000

    more

--- src/pages/gamePage.js
+++ src/pages/gamePage.js
@@ -15,20 +15,34 @@ const GamePage = () => {
 		setPlayersNum(event.target.value);
 	};
 
+	console.log('RESUTS',playersNum );
 	console.log('RESUTS',racersNum );
+
 
 	
 	return (
 		<>
 			<section className='d-flex justify-content-center align-items-center flex-column'>
+				{racersNum.raceMap && (<>
+					<p>Weather: {racersNum.RaceWeather}</p>
+					<p>Straigthaways: {racersNum.raceMap.straigthaways}</p>
+					<p>Turns: {racersNum.raceMap.turns}</p>
+					<p>Lap length: {racersNum.raceMap.lapLength.toFixed(2)} km</p>
+
+				</>
+				)}
+				
+
+
+				<button className='btn btn-danger w-25 m-4' onClick={startGame}>SET MAP</button>
 				<div className="input-group input-group-sm ">
+					
 					<div className="input-group-prepend">
 						<span className="input-group-text" id="inputGroup-sizing-sm">Number of players</span>
 					</div>
 					<input type="number" onChange={handlePlayerNumInput} className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
 				</div>
-				{playersNum}
-				<button className='btn btn-danger w-25 m-4' onClick={startGame}>SET PLAYERS</button>
+				
 			</section>
 		</>
 	);
